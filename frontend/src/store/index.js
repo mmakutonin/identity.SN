0ee362b155a312fd1b77853ba33666d51d0d71bf
mmakutonin <michael.makutonin@gmail.com>
2020-04-13 18:47:00 -0400

    Added some Vuex integration for AccountSetup.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -11,5 +11,40 @@ export default new Vuex.Store({
     actions: {
     },
     modules: {
+        AccountSetup: {
+            state: {
+                //dummy data
+                interestsAll:    ['Sports', 'Buffalo', 'LGBTQ+', 'Religion', 'MasterChef'],
+                interestsKnown:  [],
+                identitiesAll:   ['Buffalo', 'Bisexual', 'Jewish', 'Christian', 'Buffalo Bills'],
+                identitiesKnown: []
+            },
+            mutations: {
+                populateUserMetadata(state, payload) {
+                    state.interestsAll = payload.interestsAll
+                    state.interestsKnown = payload.interestsKnown
+                    state.identitiesAll = payload.identitiesAll
+                    state.identitiesKnown = payload.identitiesKnown
+                },
+                identityAdd(state, payload) {
+                    Vue.set(state.identitiesKnown, state.identitiesKnown.length, state.identitiesAll[payload.index])
+                },
+                identityRemove(state, payload) {
+                    Vue.delete(state.identitiesKnown, payload.index)
+                },
+                interestAdd(state, payload) {
+                    Vue.set(state.interestsKnown, state.interestsKnown.length, state.interestsAll[payload.index])
+                },
+                interestRemove(state, payload) {
+                    Vue.set(state.interestsKnown, payload.index)
+                },
+            },
+            actions: {
+                getUserMetadata({commit}, payload) {
+                    //to be implemented
+
+                }
+            }
+        }
     }
 })
--- frontend/src/views/AccountSetup.vue
+++ frontend/src/views/AccountSetup.vue
@@ -3,3 +3,17 @@
         Hi from Account Setup
     </div>
 </template>
+<script>
+import {mapState} from 'vuex'
+export default {
+    name:     'AccountSetup',
+    computed: {
+        ...mapState([
+            'interestsAll',
+            'interestsKnown',
+            'identitiesAll',
+            'identitiesKnown'
+        ])
+    }
+}
+</script>
