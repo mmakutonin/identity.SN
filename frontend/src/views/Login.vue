f54da7471efeaf7a1b9f06f3416520c6d3ae9f91
mmakutonin <michael.makutonin@gmail.com>
2020-04-13 17:20:26 -0400

    Created login web version (not mobile responsive)

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,11 +1,13 @@
 <template>
   <div class='w3-row-padding w3-center w3-indigo cust-login'>
-    <div class='w3-half '>
-      Let's Explore
-      <br />Identity
+    <div class='w3-half w3-display-container cust-full-height'>
+      <div class='w3-display-middle w3-text-white w3-round-xlarge w3-padding'>
+        <h3>Let's Explore </h3>
+        <h1> Identity </h1>
+      </div>
     </div>
-    <div class='w3-half'>
-      <div class='w3-card w3-twothird w3-white w3-round-xlarge'>
+    <div class='w3-half w3-display-container cust-full-height'>
+      <div class='w3-display-middle w3-card w3-twothird w3-white w3-round-xlarge cust-threequarter-height'>
         <LoginOAuthButton signInMethod='Google' />
         <LoginOAuthButton signInMethod='Facebook' />
       </div>
@@ -16,9 +18,9 @@
 <script>
 import LoginOAuthButton from '../components/LoginOAuthButton'
 export default {
-	name: 'Login',
-	components: {
-		LoginOAuthButton
-	}
+    name:       'Login',
+    components: {
+        LoginOAuthButton
+    }
 }
 </script>
