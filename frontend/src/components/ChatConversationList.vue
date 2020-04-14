f47a6d746db1791eeb0299affdae6916c2d4e6af
mmakutonin <michael.makutonin@gmail.com>
2020-04-14 18:37:27 -0400

    Created chat list.

--- frontend/src/components/ChatConversationList.vue
+++ frontend/src/components/ChatConversationList.vue
@@ -1,5 +1,47 @@
 <template>
     <div>
-        Conversation List Placeholder
+        <div
+            v-for='(contact, index) in contacts'
+            v-bind:key='contact.chatIndex'
+            class='w3-card w3-container w3-section w3-border'
+        >
+            <input
+                v-bind:value='contact.initials'
+                v-bind:class='classStr(index)'
+                v-on:click='changeCurrentContact({index})'
+            />
+            {{contact.lastMessage.content}}
+        </div>
     </div>
 </template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+export default {
+    name:    'ChatConversationList',
+    methods: {
+        classStr(index) {
+            const className = 'w3-btn w3-round '
+            if(index === this.currentContactIndex) {
+                return className + 'w3-blue'
+            }
+            else {
+                return className + 'w3-light-blue'
+            }
+        },
+        ...mapMutations('chat', [
+            'changeCurrentContact'
+        ])
+    },
+    computed: {
+        ...mapState('chat', {
+            contacts: state => state.contacts.map((contact) => ({
+                ...contact,
+                lastMessage: state.chats[contact.chatIndex][state.chats[contact.chatIndex].length - 1],
+                initials:    contact.name.split(' ').reduce((agg, word) => agg + word[0], '').toUpperCase()
+            })),
+            currentContactIndex: state => state.currentContactIndex
+        })
+    }
+}
+</script>
