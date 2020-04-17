a9c2e08671cdb85c3717c49cc3d97f6ae8957fc0
MidnightPineapple <samuel88835@yahoo.com>
2020-04-17 19:23:04 -0400

    hotfix: retrieve messages from & to user

--- sails/api/controllers/MessageController.js
+++ sails/api/controllers/MessageController.js
@@ -28,8 +28,10 @@ module.exports = {
     // TODO: replace w/ user who's logged in
     const user = (await User.find())[0];
 
-    const messages = await Message.find({ from: user.id })
+    const messages = await Message.find({
+      or: [{ from: user.id }, { to: user.id }],
+    });
 
-    return res.json(messages)
+    return res.json(messages);
   },
 };
