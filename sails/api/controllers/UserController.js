75b8f2516bbcbee7df0c482a1331884216d7a291
Daniela Eidelman <deidelma@gmu.edu>
2020-04-20 20:23:11 -0400

    Implemented user model creation

--- sails/api/controllers/UserController.js
+++ sails/api/controllers/UserController.js
@@ -1,9 +1,9 @@
 /**
- * UserControllerController
- *
- * @description :: Server-side actions for handling incoming requests.
- * @help        :: See https://sailsjs.com/docs/concepts/actions
- */
+* UserControllerController
+*
+* @description :: Server-side actions for handling incoming requests.
+* @help        :: See https://sailsjs.com/docs/concepts/actions
+*/
 
 // TODO: make sure the user actually has permission to do these operations
 // TODO: then we wont need to include the weird user email / id thing in the urlstring
@@ -11,92 +11,116 @@
 var passport = require('passport');
 
 module.exports = {
-  // Expects the id parameter to contain an email address
-  // So escape the . in the email address with %2E
-  find: async function (req, res) {
-    const { id } = req.params;
-    const user = await User.findOne({ id });
+    // Expects the id parameter to contain an email address
+    // So escape the . in the email address with %2E
+    find: async function (req, res) {
+        const { id } = req.params;
+        const user = await User.findOne({ id });
 
-    return res.json(user);
-  },
+        return res.json(user);
+    },
 
-  store: async function (req, res) {
-    // Should only be usable by Google webhook & passport JS later on
+    store: async function (req, res) {
+        // Should only be usable by Google webhook & passport JS later on
 
-    const { body } = req;
+        const { body } = req;
 
-    const user = await User.create({
-      fName: body.fName,
-      lName: body.lName,
-      displayName: body.displayName,
-      id: body.email,
-    }).fetch()
+        const user = await User.create({
+            fName: body.fName,
+            lName: body.lName,
+            displayName: body.displayName,
+            id: body.email,
+        }).fetch()
 
-    return res.json(user);
-  },
+        return res.json(user);
+    },
 
-  update: async function (req, res) {
-    const { body, params } = req;
+    update: async function (req, res) {
+        const { body, params } = req;
 
-    const newAttributes = {};
+        const newAttributes = {};
 
-    if (typeof body.displayName !== "undefined") {
-      newAttributes.displayName = body.displayName;
-    }
+        if (typeof body.displayName !== "undefined") {
+            newAttributes.displayName = body.displayName;
+        }
 
-    if (typeof body.email !== "undefined") {
-      newAttributes.id = body.email;
-    }
+        if (typeof body.email !== "undefined") {
+            newAttributes.id = body.email;
+        }
 
-    if(typeof body.fName !== "undefined") {
-      newAttributes.fName = body.fName;
-    }
+        if(typeof body.fName !== "undefined") {
+            newAttributes.fName = body.fName;
+        }
 
-    if(typeof body.fName !== "undefined") {
-      newAttributes.lName = body.lName;
-    }
+        if(typeof body.fName !== "undefined") {
+            newAttributes.lName = body.lName;
+        }
 
-    const user = await User.update({ id: params.id })
-      .set(newAttributes)
-      .fetch();
+        const user = await User.update({ id: params.id })
+        .set(newAttributes)
+        .fetch();
 
-    return res.json(user);
-  },
+        return res.json(user);
+    },
 
-  destroy: async function (req, res) {
-    const { id } = req.params;
+    destroy: async function (req, res) {
+        const { id } = req.params;
 
-    await User.destroy({ id });
+        await User.destroy({ id });
 
-    return res.ok();
-  },
+        return res.ok();
+    },
 
-  googleAuth: function(req, res) {
-    passport.authenticate('google', { scope: ['email', 'profile'] })(req, res);
-  },
+    googleAuth: function(req, res) {
+        passport.authenticate('google', { scope: ['email', 'profile'] })(req, res);
+    },
 
-  googleCallback: function(req, res, next) {
-    passport.authenticate('google', function(err, user) {
-      if(err) {
-        res.redirect('back')
-      } else {
-        const newUser = true //change this to programmatically set
-        res.redirect(`http://localhost:8080/auth?newuser=${newUser}&id=${user.id}`) //hard-coded for now, will change when deploying.
-      }
-    })(req, res, next);
-  },
-  facebookAuth: function(req, res, next) {
-    passport.authenticate('facebook', { scope: ['email']})(req, res, next);
-  },
+    googleCallback: function(req, res, next) {
+        passport.authenticate('google', function(err, user) {
+            if(err) {
+                res.redirect('back')
+            } else {
+                var newUser = true;
+                User.findOrCreate({ id: user.email }, { id: user.email, fullName: user.firstname + " " + user.last_name, displayName: user.firstname + " " + user.last_name})
+                .exec(async(err, user, wasCreated)=> {
+                    if (err) { return res.serverError(err); }
 
-  facebookCallback: function(req, res, next) {
-    passport.authenticate('facebook', function(err, user) {
-      if(err) {
-        res.redirect('back')
-      } else {
-        const newUser = true //change this to programmatically set
-        res.redirect(`http://localhost:8080/auth?newuser=${newUser}&id=${user.id}`) //hard-coded for now, will change when deploying.
-      }
-    })(req, res, next);
-  },
+                    if(wasCreated) {
+                        console.log("User created");
+                    }
+                    else {
+                        newUser = false;
+                        console.log("User found");
+                    }
+                });
+                res.redirect(`http://localhost:8080/auth?newuser=${newUser}&id=${user.id}`) //hard-coded for now, will change when deploying.
+            }
+        })(req, res, next);
+    },
+    facebookAuth: function(req, res, next) {
+        passport.authenticate('facebook', { scope: ['email']})(req, res, next);
+    },
+
+    facebookCallback: function(req, res, next) {
+        passport.authenticate('facebook', function(err, user) {
+            if(err) {
+                res.redirect('back')
+            } else {
+                var newUser = true;
+                User.findOrCreate({ id: user.email }, { id: user.email, fullName: user.firstname + " " + user.last_name, displayName: user.firstname + " " + user.last_name})
+                .exec(async(err, user, wasCreated)=> {
+                    if (err) { return res.serverError(err); }
+
+                    if(wasCreated) {
+                        console.log("User created");
+                    }
+                    else {
+                        newUser = false;
+                        console.log("User found");
+                    }
+                });
+                res.redirect(`http://localhost:8080/auth?newuser=${newUser}&id=${user.id}`) //hard-coded for now, will change when deploying.
+            }
+        })(req, res, next);
+    },
 };
