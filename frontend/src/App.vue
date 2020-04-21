2be277a9dbd552fb7552b7f730acf6023820bcbf
mmakutonin <michael.makutonin@gmail.com>
2020-04-21 17:12:04 -0400

    implemented Dark Mode.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view/>
+  <router-view v-bind:class='bgColor'/>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['bgColor'])
+  }
+}
+</script>
--- frontend/src/components/MainNavBar.vue
+++ frontend/src/components/MainNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w3-bar w3-border-bottom w3-border-red w3-mobile navbar">
+  <div v-bind:class="'w3-bar w3-border-bottom w3-border-red w3-mobile navbar ' + bgColor">
     <div class="nav-logo"><img src="../assets/ic-icon.png" /></div>
 
     <div class="nav-items">
@@ -23,12 +23,23 @@
         class="w3-bar-item w3-button w3-hover-none w3-hover-text-indigo w3-round"
         >Contact</a
       >
+      <input
+        type='button'
+        class='w3-bar-item w3-button w3-hover-none w3-hover-text-indigo w3-round'
+        value='Settings'
+        v-on:click='$router.push({path: "/settings"})'
+      />
     </div>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
+
 export default {
   name: "MainNavBar",
+  computed: {
+    ...mapState(['bgColor'])
+  }
 };
 </script>
